c013c849c344cccf1d0d04ef5d65c981b969aad6
João Gabriel Sasseron <93351447+Sasseronj@users.noreply.github.com>
2022-07-15 11:32:38 -0300

    att colocação

--- Codigo/Grupo.py
+++ Codigo/Grupo.py
@@ -8,7 +8,11 @@ class Grupo():
         self.selecao2 = Selecao(df_selecoes[grupo][1])
         self.selecao3 = Selecao(df_selecoes[grupo][2])
         self.selecao4 = Selecao(df_selecoes[grupo][3])
-    
+        self.selecao1.set_colocacao(2)
+        self.selecao1.set_colocacao(3)
+        self.selecao1.set_colocacao(4)
+        self.selecao1.set_colocacao(5)
+
     def teste(self):
         print(self.selecao1)
         print(self.selecao2)
@@ -22,10 +26,10 @@ class Grupo():
         lista_grupos.sort(key=lambda x: x.saldo_de_gols, reverse=True)
         lista_grupos.sort(key=lambda x: x.pontos       , reverse=True)
         
-        self.selecao1.set_colocacao(lista_grupos.index(self.selecao1)+1)
-        self.selecao2.set_colocacao(lista_grupos.index(self.selecao2)+1)
-        self.selecao3.set_colocacao(lista_grupos.index(self.selecao3)+1)
-        self.selecao4.set_colocacao(lista_grupos.index(self.selecao4)+1)
+        self.selecao1.set_colocacao(lista_grupos.index(self.selecao1)+2)
+        self.selecao2.set_colocacao(lista_grupos.index(self.selecao2)+2)
+        self.selecao3.set_colocacao(lista_grupos.index(self.selecao3)+2)
+        self.selecao4.set_colocacao(lista_grupos.index(self.selecao4)+2)
 
             
 if __name__ == "__main__":
